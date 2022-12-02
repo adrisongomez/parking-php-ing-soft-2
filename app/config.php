ad27b1da2c56b002378ac22234b15c5749dd3e33
Adrison Gomez <info@adrisongomez.me>
2022-12-02 21:13:07 +0000

    Fixing errors in Docker configurations

--- app/config.php
+++ app/config.php
@@ -1,24 +1,24 @@
 <?php
 
 
-define('SERVIDOR',$_ENV['DATABASE_DOMAIN']);
-define('USUARIO',$_['DATABASE_USER']);
-define('PASSWORD',$_ENV['DATABASE_PASSOWRD']);
-define('BD',$_ENV['DATABASE_NAME']);
+define('SERVIDOR', $_ENV['DATABASE_DOMAIN']);
+define('USUARIO', getenv('DATABASE_USERNAME'));
+define('PASSWORD', getenv('DATABASE_USERPASSOWRD'));
+define('BD', $_ENV['DATABASE_NAME']);
 define('PORT', $_ENV['DATABASE_PORT']);
 
-$servidor="mysql:dbname=".BD.";host=".SERVIDOR.";port:".PORT;
+$servidor = "mysql:dbname=" . BD . ";host=" . SERVIDOR . ";port:" . PORT;
 
-try{
-    $pdo = new PDO($servidor,USUARIO,PASSWORD,array(PDO::MYSQL_ATTR_INIT_COMMAND=>"SET NAMES utf8"));
+try {
+    $pdo = new PDO($servidor, USUARIO, PASSWORD, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8")); 
     //echo "La conexión a la base de datos fue exitosa";
-}catch (PDOException $e){
+} catch (PDOException $e) {
     // echo "Error a la base de datos";
-    echo "<script>alert('Error en la conexión a la base de datos');</script>";
+    echo "<script>alert('Error en la conexión a la base de datos 1');</script>";
 }
 
 
-$URL='http://localhost/www.sisparqueo.com';
+$URL = 'http://localhost/www.sisparqueo.com';
 
 $estado_del_registro = "1";
 
