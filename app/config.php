754372b13b2a8670a4874b36665acc3a3c96bcee
Adrison Gomez <agomez@resonance.nyc>
2022-12-03 07:40:44 -0400

    Fail error

--- app/config.php
+++ app/config.php
@@ -10,13 +10,7 @@ define('PORT', $_ENV['DATABASE_PORT']);
 $servidor = "mysql:dbname=" . BD . ";host=" . SERVIDOR . ";port=" . PORT;
 
 
-try {
-    $pdo = new PDO($servidor, USUARIO, PASSWORD, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
-    //echo "La conexión a la base de datos fue exitosa";
-} catch (PDOException $e) {
-    // echo "Error a la base de datos";
-    echo "<script>alert('Error en la conexión a la base de datos');</script>";
-}
+$pdo = new PDO($servidor, USUARIO, PASSWORD, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
 
 
 $URL = getenv("DOMAIN");
